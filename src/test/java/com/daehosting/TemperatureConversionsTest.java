251877782ea281a1ef6027856863c958a40e7dc7
EliseJava <elisestrauss999@gmail.com>
2018-03-10 09:32:14 -0600

    Week 8 - activity 2 Create Repro

---
+++ src/test/java/com/daehosting/TemperatureConversionsTest.java
@@ -0,0 +1,18 @@
+package com.daehosting;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+
+public class TemperatureConversionsTest {
+
+    @Test
+    public void getTemperatureConversionsSoap12() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.celsiusToFahrenheit(BigDecimal.valueOf(0));
+        assertEquals(32,result);
+    }
+}
